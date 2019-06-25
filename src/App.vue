9dea56d3204627a2921f1f14a2bb680dee982a15
matthiasleutard <matthias.leutard@adfab.fr>
2019-06-25 10:29:29 +0200

    envoi du projet cv

---
+++ src/App.vue
@@ -0,0 +1,97 @@
+<template>
+  <div id="app">
+    <h1>Mon cv</h1>
+        <div class="element-jake" id="jake">
+            
+            <a href="#"><img @click="adventure" src="https://img.icons8.com/color/100/000000/jake.png"></a>
+            <span class="element-jake-hide">{{ randomPhrase }}</span>
+        </div>
+        <div class="container">
+            <div class="container-infos-profil">
+                <br>
+                <div class="photo-profile">
+                    <img src="./assets/img/profil_cat.jpg" alt="cat-profil">
+                </div>
+                <div class="infos-infos">
+                    <h3>Infos</h3>
+                    <p>Developpeur web junior FRONT</p>
+                </div>
+                <div class="contact-infos" id="profilContact">
+                    <h3>Contact</h3>
+                    <p>Phone : {{ phone }}</p>
+                    <p>Mail : {{ mail }}</p>
+                </div>
+                <div class="outil-infos">
+                    <h3>Stuff</h3>
+                    <p>Html/Css, Javascript, scss, Vuejs</p>
+                </div>
+                <br>
+                <div class="lien-click">
+                    <h3>Liens</h3>
+                    <br>
+                    <a href="https://www.instagram.com/"><img src="https://img.icons8.com/color/50/000000/treasure-chest.png"></a>
+                    <a href="https://www.youtube.com/"><img src="https://img.icons8.com/color/50/000000/sword.png"></a>
+                    <a href="http://www.jeuxvideo.com/"><img src="https://img.icons8.com/color/48/000000/kawaii-coffee.png"></a>
+                </div>
+            </div>
+            <div class="container-infos-xp">
+                    <Info title="Expérience" :infos="experience" />
+                    <Info title="Éducation" :infos="education" />
+                    <Info title="Loisirs" :infos="loisir" />
+            </div>
+        </div>
+  </div>
+</template>
+
+<script>
+import Info from './components/Info.vue'
+import './assets/scss/index.scss';
+
+export default {
+  name: 'app',
+  components: {
+    Info
+  },
+  data() {
+    return {
+      phrase: ['Don\'t touch me anymore', 
+                'TU tu TU tu TU tu tu TU',
+                'Look at my poweEeeEer', 
+                'Oh my doggyDog ?!'
+              ],
+        randomPhrase: '',
+        phone: '06.79.86.38.05',
+        mail: 'mleutard@gmail.com',
+        experience: [
+          { name: 'Stage de Mai 2019/juillet 2019 : ADFAB -> developpeur web FRONT junior'}
+        ],
+        education: [
+          { name: 'Ifocop -> formation developpeur web junior 2017' },
+          { name: 'OpenClassRooms -> formation developpeur web junior 2018/2019' }
+        ],
+        loisir: [
+          { name: 'Theatre' },
+          { name: 'Jeux videos' },
+          { name: 'Skateboard' }
+        ]
+    }
+  },
+  methods: {
+      adventure () {
+          let crea = document.querySelector('.element-jake span');
+
+          if (crea.classList.contains('element-jake-hide')) {
+              crea.classList.remove('element-jake-hide');
+              this.speakJake();
+          } else {
+              crea.classList.add('element-jake-hide');
+          }
+      },
+
+      speakJake () {
+          const index = Math.floor(Math.random() * this.phrase.length);
+          this.randomPhrase = this.phrase[index];
+      }
+  }
+}
+</script>
---
+++ src/components/Info.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="element-infos-xp">
+        <h3>{{ title }}</h3>
+        <ul>
+            <li v-for="todo in infos" :key="todo.name">
+                <br>
+                {{ todo.name }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Info',
+  props: [
+      'infos',
+      'title'
+  ],
+  mounted() {
+      console.log(this.infos)
+  }
+}
+</script>
